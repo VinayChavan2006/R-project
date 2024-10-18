5114c30f8fca7cb5c2c1cf46c750306f6fe499a7
Vinay Chavan <vinaychavan2006@gmail.com>
2024-10-18 13:08:33 +0530

    Adding olympic R file

---
+++ Data/Olympics.R
@@ -0,0 +1,5 @@
+# Olympics Data
+
+library(dplyr)
+load(url("https://raw.githubusercontent.com/VinayChavan2006/R-project/main/Data/OlympicAthletes.Rdata"))
+load(url("https://raw.githubusercontent.com/VinayChavan2006/R-project/main/Data/OlympicMedals.Rdata"))
